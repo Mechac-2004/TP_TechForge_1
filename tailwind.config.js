463614218a56aa34c854ebaeaba001770a021f92
ASSONGBA Méchac <mechacassongba68@gmail.com>
2025-08-07 00:22:32 +0200

    Three commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+content: ["./*.{html,js}"],
+theme: {
+extend: {
+fontFamily: {
+'sans': ['Helvetica', 'Arial', 'sans-serif'],
+},
+colors: {
+'primary': '#2563eb',
+'secondary': '#6b7280',
+'neutral-100': '#f3f4f6',
+'neutral-200': '#e5e7eb',
+},
+spacing: {
+'8xl': '96rem',
+'9xl': '128rem',
+},
+borderRadius: {
+'4xl': '2rem',
+}
+},
+},
+plugins: [],
+}
